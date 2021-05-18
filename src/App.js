8543f2597ce5a7ec73f3422b8abaca2f5dcc2840
Angeluz100 <francotgutierrez@gmail.com>
2021-05-18 16:57:52 -0400

    API Connected

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import MovieList from './components/MovieList'
 
 function App() {
   const [movies, setMovies] = useState([ 
@@ -25,9 +27,27 @@ function App() {
     "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
   },
   ]);
+
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=star wars&apikey=23752a75"
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    console.log(responseJson)
+    setMovies(responseJson.Search)
+  };
+
+  useEffect (() => {
+    getMovieRequest();
+  }, [])
+
+
   return (
-    <div>
-      
+    <div className='container-fluid movie-app'>
+      <div className='row'>
+      <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
