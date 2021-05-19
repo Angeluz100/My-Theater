964541fb9958b659cb7e44a4c87b3902cf031583
Angeluz100 <francotgutierrez@gmail.com>
2021-05-19 10:35:35 -0400

    Adding a Favorite Component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList'
 import Heading from './components/Heading'
 import SearchBox from './components/SearchBox'
+import AddFavorites from './components/AddFavorites'
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=avengers&apikey=23752a75"
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=23752a75`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
 
-    console.log(responseJson)
-    setMovies(responseJson.Search)
+    if (responseJson.Search){
+      setMovies(responseJson.Search)
+    }
   };
 
   useEffect (() => {
-    getMovieRequest();
-  }, [])
+    getMovieRequest(searchValue);
+  }, [searchValue])
 
 
   return (
     <div className='container-fluid movie-app'>
-      <div className="row">
+      <div className="row d-flex align-items-center mt-4 mb-4">
         <Heading heading='Movies'/>
-        <SearchBox />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
       </div>
       <div className='row'>
-      <MovieList movies={movies} />
+      <MovieList movies={movies} favoriteComponent={AddFavorites}/>
       </div>
     </div>
   );
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,10 @@ import React from 'react';
 const SearchBox = (props) => {
     return (
         <div className="col col-sm-4">
-            <input className="form-control" placeholder="type to search..."/>
+            <input className="form-control" 
+            value={props.value}
+            onChange={(e)=> props.setSearchValue(e.target.value)}
+            placeholder="type to search..."/>
         </div>
     )
 };
